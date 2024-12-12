1506578b8062f1a94167860d219a9a10a6545a78
cxrlosarmando <carlos.armandor28@gmail.com>
2024-12-11 23:22:31 -0300

    Pruebas unitarias hito-3

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -3,11 +3,9 @@ import { userController } from "../controllers/users.controller";
 import { verifyToken } from "../midlleware/authMiddleware";
 
 const router = Router();
-
-
 router.get("/:id", userController.getUsers);
-router.put("/:id", userController.updateUser);
-router.delete("/:id", verifyToken, userController.deleteUser);
+router.put("/api/update/:id", userController.updateUser);
+router.delete("/api/delete", verifyToken, userController.deleteUser);
 router.post("/", userController.createUser);
 
 export default router;
