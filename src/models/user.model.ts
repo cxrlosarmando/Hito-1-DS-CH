f83504700540b03fb4738fa25f443f81522d8d76
cxrlosarmando <carlos.armandor28@gmail.com>
2025-01-30 18:34:00 -0300

    Correciones del proyecto a entregar en el modelo y services

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,52 +1,43 @@
 import { sequelize } from '../config/sequelize';
-import { DataTypes } from 'sequelize';
+import { DataTypes, Model, Optional } from "sequelize";
+
+interface IUser {
+  id?: number;
+  email: string;
+  password: string;
+}
 
 
-const User = sequelize.define('User', {
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
+interface IUserCreationAttributes extends Optional<IUser, "id"> {}
+class User extends Model<IUser, IUserCreationAttributes> implements IUser {
+  public id!: number;
+  public email!: string;
+  public password!: string;
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: { isEmail: true },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-}, {
-  timestamps: true, 
-  tableName: 'users',
-});
-
-const getUserByEmail = async (email: string) => {
-  const user = await User.findOne({ where: { email } });
-  console.log(user);
-  return user;
-};
-
-const create = async (email: string, password: string) => {
-  const newUser = await User.create({ email, password });
-  console.log(newUser);
-  return newUser;
-};
-
-const remove = async (email: string) => {
-  const deletedUser = await User.destroy({ where: { email } });
-  console.log(deletedUser);
-  return deletedUser;
-};
-
-const update = async (id: string, email: string, password: string) => {
-  const updatedUser = await User.update(
-    { email, password },
-    { where: { id }, returning: true }
-  );
-  console.log(updatedUser);
-  return updatedUser;
-};
-
-export const UserModel = {
-  create,
-  getUserByEmail,
-  remove,
-  update,
-};
+  {
+    sequelize,
+    modelName: "User",
+    tableName: "users",
+    timestamps: true,
+  }
+);
+export default User;
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,47 +1,61 @@
 import bcrypt from "bcryptjs";
-import { UserModel } from "../models/user.model";
+import  User  from "../models/user.model"; 
 
 
-const getAllUsers = async (email: string,) =>{
-  const users= await UserModel.getUserByEmail(email);
-  if(!users) throw new Error("User not found");
+const getAllUsers = async () => {
+  const users = await User.findAll();
   return users;
-} 
-const createUserWithEmailAndPassword = async (email: string, password: string) => {
-  const user = await UserModel.getUserByEmail(email);
-
-  if (user) throw new Error("User already exists");
-
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-  const newUser = await UserModel.create(email, hashedPassword);
-  return newUser;
 };
 
-const deleteUserById = async (id: string) => {
-  const user = await UserModel.getUserByEmail(id);
 
-  if (!user) throw new Error("User not found");
-
-  const deletedUser = await UserModel.remove(id);
-  return deletedUser;
-};
-
-const updateUserById = async (id: string, email: string, password: string) => {
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-  const updatedUser = await UserModel.update(id, email, hashedPassword);
-  return updatedUser;
-};
 const getUserByEmail = async (email: string) => {
-  const user = await UserModel.getUserByEmail(email);
+  const user = await User.findOne({ where: { email } });
+
   if (!user) throw new Error("User not found");
   return user;
 };
+
+
+const createUserWithEmailAndPassword = async (email: string, password: string) => {
+  const userExists = await User.findOne({ where: { email } });
+
+  if (userExists) throw new Error("User already exists");
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const newUser = await User.create({ email, password: hashedPassword }); // Pasamos un objeto
+  return newUser;
+};
+
+
+const deleteUserById = async (id: string) => {
+  const user = await User.findByPk(id); 
+
+  if (!user) throw new Error("User not found");
+
+  await user.destroy(); 
+  return { message: "User deleted successfully" };
+};
+
+
+const updateUserById = async (id: string, email: string, password: string) => {
+  const user = await User.findByPk(id);
+
+  if (!user) throw new Error("User not found");
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  await user.update({ email, password: hashedPassword });
+
+  return user;
+};
+
 export const userService = {
   createUserWithEmailAndPassword,
   deleteUserById,
   updateUserById,
   getAllUsers,
-  getUserByEmail
+  getUserByEmail,
 };
